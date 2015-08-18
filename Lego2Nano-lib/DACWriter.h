a620804feef48f5ffed0f0881e20b643c9b188a5
Vertex-YC <vertexthm@163.com>
2015-08-18 16:37:23 +0800

    upload DACWriter

--- Lego2Nano-lib/DACWriter.h
+++ Lego2Nano-lib/DACWriter.h
@@ -1,24 +1,45 @@
-// DACWriter.h
+	// DACWriter.h
 
-#ifndef _DACWRITER_h
-#define _DACWRITER_h
+	#ifndef _DACWRITER_h
+	#define _DACWRITER_h
 
-#if defined(ARDUINO) && ARDUINO >= 100
-	#include "arduino.h"
-#else
-	#include "WProgram.h"
-#endif
+	#if defined(ARDUINO) && ARDUINO >= 100
+		#include "arduino.h"
+	#else
+		#include "WProgram.h"
+	#endif
 
-class DACWriterClass
-{
- protected:
+	class DACWriterClass
+	{
+	 protected:
 
+		 int stepSize;
+		 bool rng;
 
- public:
-	void init();
-};
+		 int dataPin;
+		 int clkPin;
+		 int loadPin;
+		 int ldacPin;
 
-extern DACWriterClass DACWriter;
+		 int currentX;
+		 int currentY;
+		 int currentZ;
 
-#endif
+	 public:
+		void init(int, int, int, int, int, bool, unsigned long);
+		void scanLine();
+		int stepUp(int);
+		int stepDn(int);
+		int reset(int);
+		int move(int);
+
+	private:
+		int sendHigh();
+		int sendLow();
+		int load();
+	};
+
+	extern DACWriterClass DACWriter;
+
+	#endif
 
