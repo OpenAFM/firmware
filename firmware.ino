f97aed765ef45ab2172096c29e0f1f0b8d1ee130
Vertex-YC <vertexthm@163.com>
2015-08-18 16:31:36 +0800

    frame built

--- firmware.ino
+++ firmware.ino
@@ -1,72 +1,24 @@
 #include "DACWriter.h"
-<<<<<<< HEAD
 #include "DataRecv.h"
-=======
-//#include "DataRecv.h"
->>>>>>> 298263cbfb8e117f515ae14f4bc1a5731d82ba98
 
 /* Pin Definitions */
-#define SDI 2;
-#define SCK 3;
-#define LOAD 4;
-#define LDAC 5;
-<<<<<<< HEAD
+#define SDI 2
+#define SCK 3
+#define LOAD 4
+#define LDAC 5
 #define RECVPin A7
+
+#define pixelCount 8
+
+int detectTimes = 10; // Parameter
+int pixelData[pixelCount] = {0};
 
 DACWriterClass WRITER;
 DataRecvClass RECV;
 
-int pixelCount = 8; // Parameter
-int detectTimes = 10; // Parameter
-int pixelData[pixelCount] = {0};
-
-=======
-#define RNG 0;
-//#define DataRecvPin ?
-
-//#define pixelCount ? // Pixels in one line
-//#define detectTimes ? // Samples of one pixel
-
-
-DACWriterClass writer;
-//DataRecvClass DataRecv;
-
-
-void setup() {
-      //  pinMode(DataRecvPin, INPUT);
-        
-        /* Trigger the interrupt when pin 2 changes value*/
-        /* See "https://www.arduino.cc/en/Reference/AttachInterrupt" */
-       // attachInterrupt(0, interrput, HIGH); 
-
-	// int stepSize, int dataPin, int clkPin, int loadPin, int ldacPin, bool rng, unsigned long serialRate
-	//writer.init(1, SDA, SCK, LOAD, LDAC, true, 115200);
-		//1, SDI, _SCK, LOAD, LDAC, RNG, 115200);
-}
-
-void loop() {
-       /* int pixelData[pixelCount];
-    
-        for (int i = 0; i < pixelCount; ++i) {
-            	/* Call DACWriter Function */
-        	
-        	/* Call DataRecv Function */
-        //        pixelData[i] = DataRecv.detectPixel(DataRecvPin);
-        //}
-		
-	writer.move(0);
-	writer.stepUp(0);
-	delay(30);
-
-        /* Send Data */
-
-}
->>>>>>> 298263cbfb8e117f515ae14f4bc1a5731d82ba98
-
 void interrupt() {
     delay(10);
 }
-<<<<<<< HEAD
 
 void dataSend(int* array) {
     for (int i = 0; i < pixelCount; ++i) {
@@ -84,7 +36,9 @@ void initialize() {
 void startWork() {
     for (int i = 0; i < pixelCount; ++i) {
         /* Call DACWriter Function */
-    	
+	WRITER.move(0);
+	WRITER.stepUp(0);
+	delay(30);
     	/* Call DataRecv Function */
         pixelData[i] = RECV.detectPixel(RECVPin, detectTimes);
     }
@@ -94,7 +48,6 @@ void startWork() {
 
 void setParameter() {
 }
-
 
 void setup() {
     dataSend(pixelData);
@@ -114,5 +67,3 @@ void loop() {
         delay(1);
     }
 }
-=======
->>>>>>> 298263cbfb8e117f515ae14f4bc1a5731d82ba98
---
+++ test/test.ino
@@ -0,0 +1,23 @@
+#include "DataRecv.h"
+
+#define DataRecvPin A7
+#define pixelCount 1
+#define detectTimes 10 // Samples of one pixel
+
+DataRecvClass RECV;
+
+void setup() {
+        Serial.begin(9600);
+        pinMode(DataRecvPin, INPUT);
+}
+
+void loop() {
+        int pixelData[pixelCount];    
+        for (int i = 0; i < pixelCount; ++i) {
+                pixelData[i] = RECV.detectPixel(DataRecvPin, detectTimes);
+                Serial.println(pixelData[i]);
+                delay(100);
+        }
+        delay(1000);
+}
+
