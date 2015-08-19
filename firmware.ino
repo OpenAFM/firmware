186c47fbd14fe66d678d2057831e455573355737
天琪 唐 <ttq1008@tens-MacBook-Pro.local>
2015-08-19 11:53:02 +0800

    more elegant

--- firmware.ino
+++ firmware.ino
@@ -6,21 +6,77 @@
 #define SCK 3
 #define LOAD 4
 #define LDAC 5
+
+#define STEPSIZE 1
+#define RNG 0
+#define BAUDRATE 9600
+
 #define RECVPin A7
 
-#define pixelCount 8 // Pixels in one line
+#define MAX 10000
 
+int pixelCount; // Parameter, Pixels in one line
+int lineCount; // Parameter, The line number we will scan 
 int detectTimes = 10; // Parameter, Times to detect one pixel
-int pixelData[pixelCount] = {0};
+int pixelData[MAX] = {0};
 
-DACWriterClass WRITER;
+DACWriterClass SCANNER;
 DataRecvClass RECV;
+
+/* ================ */
 
 void interrupt() {
     delay(10);
 }
 
-void dataSend(int* array) {
+/* ================ */
+
+void initialize() {
+}
+
+/* ================ */
+
+void startWork() {
+    ScanOneLine();
+    SendData(pixelData);
+}
+
+void MoveToPreviousPixel(){
+    SCANNER.move(0);
+    SCANNER.stepDn(0);
+}
+
+void MoveToNextPixel(){
+    SCANNER.move(0);
+    SCANNER.stepUp(0);
+}
+
+void MoveToNextLine(){
+    SCANNER.move(1);
+    SCANNER.stepUp(1);
+}
+
+void ScanOneLine() {
+    for (int i = 0; i < pixelCount; ++i) {
+        /* Call DACWriter Function */
+        MoveToNextPixel();
+        delay(25);
+        /* Call DataRecv Function */
+        pixelData[i] = RECV.detectPixel(RECVPin, detectTimes); // calculate the avg for one pixel for one-direction scan
+        delay(25);
+    }
+    for (int i = pixelCount - 1; i > -1; --i) {
+        /* Call DACWriter Function */
+        MoveToPreviousPixel();
+        delay(25);
+        /* Call DataRecv Function */
+        pixelData[i] += RECV.detectPixel(RECVPin, detectTimes); // calculate the avg for one pixel for one-direction scan
+        pixelData[i] /= 2;
+        delay(25);
+    }
+}
+
+void SendData(int* array) {
     for (int i = 0; i < pixelCount; ++i) {
         Serial.print(array[i]);
         if (i == pixelCount - 1)
@@ -30,28 +86,22 @@ void dataSend(int* array) {
     }
 }
 
-void initialize() {
-}
-
-void startWork() {
-    for (int i = 0; i < pixelCount; ++i) {
-        /* Call DACWriter Function */
-	WRITER.move(0);
-	WRITER.stepUp(0);
-	delay(25);
-    	/* Call DataRecv Function */
-        pixelData[i] = RECV.detectPixel(RECVPin, detectTimes);
-        delay(25);
-    }
-    /* Send Data */
-    dataSend(pixelData);
-}
+/* ================ */
 
 void setParameter() {
+    pixelCount = ConvertStrToInt(Serial.readStringUntil(','));
+    lineCount = ConvertStrToInt(Serial.readStringUntil(';'));
 }
 
+int ConvertStrToInt(String str) {
+    
+}
+
+/* ================ */
+
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(BAUDRATE);
+    SCANNER.init(STEPSIZE, SDI, SCK, LOAD, LDAC, RNG, BAUDRATE);
     pinMode(RECVPin, INPUT);
     /* Trigger the interrupt when pin 2 changes value*/
     /* See "https://www.arduino.cc/en/Reference/AttachInterrupt" */
@@ -62,8 +112,15 @@ void loop() {
     if (Serial.available()) {
         String command = Serial.readStringUntil(';'); // ';' Stands for the end of a command
         if (command == "IN") initialize();
-        else if (command == "SW") startWork();
         else if (command == "SP") setParameter();
+        else if (command == "SW") {
+            for (int i = 0; i < lineCount; ++i) {
+                startWork(); // work for one line
+                MoveToNextLine();
+                if (i == lineCount - 1)
+                    Serial.print("FINISH;");
+            }          
+        }
         delay(1);
     }
 }
