3c5f14feee057732d0aaf33a668020970f11e37c
rmaptbu <rmaptbu@ucl.ac.uk>
2015-08-25 10:55:28 +0800

    added comments

--- RTx.cpp
+++ RTx.cpp
@@ -1,14 +1,8 @@
-// 
-// 
-// 
-
 #include "RTx.h"
 
-RTx::RTx()
-{
+RTx::RTx() {
 	this->inputCount = 0;
 	this->outputCount = 0;
-
 }
 
 void RTx::reset() {
@@ -18,7 +12,7 @@ void RTx::reset() {
 
 int RTx::sendData(int pixels[], int pixelCount){
   String cmd;
-  while (true){
+  while (true){ //listen until command arrives
   cmd=listen();
   if (cmd=="RDY" || cmd =="DONE"){break;}
   }
@@ -26,17 +20,17 @@ int RTx::sendData(int pixels[], int pixelCount){
   //sending data
   for (int i = 0; i < pixelCount; ++i) {
     Serial.print(pixels[i]);
-    if (i == pixelCount - 1){
-      Serial.print(';');
-      Serial.flush();}// Send ';' when all numbers are sent
+    if (i == pixelCount - 1)
+      Serial.print(';'); // Send ';' when all numbers are sent
     else
       Serial.print(','); // Send ',' when a number is sent
   }
-
+  Serial.flush();
   if (cmd=="DONE") return 0;
   else return 1;
 }
 
+//only send message if other side is ready for it
 void RTx::sendString(String message){
   String cmd;
   while (true){
@@ -55,7 +49,8 @@ String RTx::listen() {
     if (Serial.available()) {
     delay(40); //wait for entire serial to arrive
     retVal = Serial.readStringUntil(';'); //stop at ;
-    Serial.print(retVal);
+    //return input value
+    Serial.print(retVal); 
     Serial.print(';');
     Serial.flush();
       break;
--- firmware.ino
+++ firmware.ino
@@ -3,55 +3,41 @@
 #include "RTx.h"
 
 /* Pin Definitions */
-#define _SDI 12
-#define _SCK 11
-#define LOAD 9
-#define LDAC 10
+//Pins for DAC (tlc5620)
+#define _SDI 12 //Data output pin (this number will be converted to analog)(MSB first)
+#define _SCK 11 //Clock
+#define LOAD 9  //Load HIGH to clock data into DAC
+#define LDAC 10 //ldac HIGH to stop DAC output while clocking. 
+#define RNG 0   //HIGH doubles the output voltage
+//Imaging parameters
+#define STEPSIZE 1      //Stepsize of DAC(linelength/stepsize=number of pixel per line)
+#define LINE_LENGTH 256 //Voltage range over (MAX256). Defines size of image in real space
+#define SAMPLE_SIZE 5   //Number of samples taken at each pixel. Median is taken as true value
+//Communication parameters
+#define BAUDRATE 9600   //Serial interfaces communication speed (bps)
+#define SAMPLER_PIN_A A0  //Input A
+#define SAMPLER_PIN_B A1  //Input B 
 
-#define STEPSIZE 1
-#define RNG 0
-#define BAUDRATE 9600
-#define LINE_LENGTH 256
-#define SAMPLE_SIZE 5
-
-#define SAMPLER_PIN_A A0
-#define SAMPLER_PIN_B A1
-
+/* Setup */
+//Recieve/Transfer protocol
 RTx phone = RTx();
+//DAC controller (moves stage)
 DACController ctrl = DACController(STEPSIZE, LINE_LENGTH, _SDI, _SCK, LOAD, LDAC, RNG);
+//Signal sampler (detects pixels)
 SignalSampler sampler = SignalSampler(SAMPLER_PIN_A, SAMPLER_PIN_B, SAMPLE_SIZE);
+//Scanner (moves, samples, sends data)
 Scanner scanner = Scanner(ctrl, sampler, phone, LINE_LENGTH);
 
-/* ================ */
-void initialize() {Serial.println("got it;");
-}
 
-
-void scanLine() {
-	  scanner.start();
-		
-}
-/* ================ */
-
-void setParameter() {
-	//pixelCount = ConvertStrToInt(Serial.readStringUntil(','));
-	//lineCount = ConvertStrToInt(Serial.readStringUntil(';'));
-}
-
-int ConvertStrToInt(String str) {
-	return 0;
-}
-
-/* ================ */
-
+//This function runs once, when the arduino starts
 void setup() {
 	Serial.begin(BAUDRATE);
 }
 
+//This function keeps looping
 void loop() {
-  String cmd=phone.listen();
-  delay(2000);
-  if (cmd=="GO"){scanLine();}
+  String cmd=phone.listen(); //wait for command  
+  if (cmd=="GO") scanner.start(); //start scanning
   else if (cmd="ERROR"){;}
 
 }
